db2df4b7628cec3717c99243377dd95d38c6c446
Carl Whittaker <carl.whittaker@unboxedconsulting.com>
2013-02-18 16:46:00 +0000

    Add textures for a handful of languages, reorganise shaders

--- config.example.js
+++ config.example.js
@@ -1,8 +1,18 @@
-// config.js
-modules.exports = {
+module.exports = {
   host: 'ws://localhost:8080',
   orbitRadius: 2000,
   github: {
     username: 'carlmw'
+  },
+  languages: {
+    JavaScript: {
+      texture: 'textures/javascript.jpg'
+    },
+    Python: {
+      texture: 'textures/python.jpg'
+    },
+    Ruby: {
+      texture: 'textures/ruby.jpg'
+    }
   }
 };
