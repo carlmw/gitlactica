0eaa08dfd1e7420facf3c631f28ab0d1066a4ce0
Carl Whittaker <carl.whittaker@unboxedconsulting.com>
2013-02-18 12:34:35 +0000

    Use circle pack to allocate positions of planets, add a mouse wheel handler for debugging

--- test/orbit_allocator.js
+++ test/orbit_allocator.js
@@ -1,3 +1,5 @@
+var packStub = sinon.stub();
+
 mockery.registerMock('../config', {
   orbitRadius: 2000
 });
@@ -8,12 +10,35 @@ mockery.registerMock('seed-random', function () {
   };
 });
 
+mockery.registerMock('d3', {
+  layout: {
+    pack: packStub
+  }
+});
+
 mockery.registerAllowable('../lib/orbit_allocator');
 
 describe('OrbitAllocator', function () {
+  function Planet (x, y) {
+    this.mesh = {
+      position: {
+        x: x,
+        y: y
+      }
+    };
+  }
+
   var OrbitAllocator;
 
   beforeEach(function () {
+    var layoutStub = {
+      size: sinon.stub(),
+      nodes: sinon.stub().returns([])
+    };
+    layoutStub.size.returns(layoutStub);
+
+    packStub.returns(layoutStub);
+
     mockery.enable();
     OrbitAllocator = require('../lib/orbit_allocator');
   });
@@ -22,19 +47,48 @@ describe('OrbitAllocator', function () {
     mockery.disable();
   });
 
-  describe('#next', function () {
-    it('increments x', function () {
-      var allocator = new OrbitAllocator();
+  it("creates a circle pack layout with the correct dimensions", function () {
+    var sizeMock = packStub.returnValue.size = sinon.mock();
 
-      allocator.next('some/repo').x.should.equal(2000);
-      allocator.next('some/repo').x.should.equal(4000);
-    });
+    sizeMock
+      .withArgs([(2000 * 4) * 2, (2000 * 4) * 2]);
 
-    it('seeds r with name', function () {
-      var allocator = new OrbitAllocator();
+    new OrbitAllocator(['herp', 'derp', 'foo', 'bar']);
 
-      allocator.next('some/repo').r.should.equal(Math.PI);
-    });
+    sizeMock.verify();
   });
 
+  describe("#allocate", function () {
+
+    it("adds nodes to the layout", function () {
+      var nodesMock = packStub.returnValue.nodes = sinon.mock();
+
+      nodesMock
+        .withArgs({ children: [
+          { value: 1, label: 'herp', comparitor: 0.5 },
+          { value: 1, label: 'derp', comparitor: 0.5 }
+        ]})
+        .returns([{}, { x: 100, y: 100 }, { x: 100, y: 100 }]);
+
+      new OrbitAllocator(['herp', 'derp'])
+        .allocate([new Planet(100, 100), new Planet(200, 200)]);
+
+      nodesMock.verify();
+    });
+
+    it("subtracts r from each point's x and y", function () {
+      var planet = new Planet(1000, 1000);
+
+      packStub.returnValue.nodes.returns([
+        {},
+        { x: 1000, y: 1000 }
+      ]);
+
+      var points = new OrbitAllocator(['herp'])
+        .allocate([planet]);
+
+      planet.mesh.position.x.should.equal(-1000);
+      planet.mesh.position.y.should.equal(-1000);
+    });
+  });
 });
