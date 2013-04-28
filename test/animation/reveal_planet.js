357c8f86298ae7562e1311e7e0be46d5cfcab64c
Carl Whittaker <carl359@gmail.com>
2013-04-28 20:42:03 +0100

    Apply a zoom in and out to the reveal planet animation

--- test/animation/reveal_planet.js
+++ test/animation/reveal_planet.js
@@ -1,20 +1,20 @@
 describe('animation/revealPlanet', function () {
   var tween = require('tween'),
       planetPosition = { x: 100, y: 100 },
-      camera = { position: { x: 200, y: 200 } },
+      camera = { position: { x: 200, y: 200, z: 100, c: 0 } },
       revealPlanet = require('../../lib/animation/reveal_planet');
 
   it("starts from the cameras current position", function () {
     sinon.spy(tween, 'Tween');
     revealPlanet(camera, planetPosition);
 
-    tween.Tween.should.have.been.calledWith(camera.position, 1e3);
+    tween.Tween.should.have.been.calledWith(camera.position);
   });
 
   it("tweens to the planet's x + y", function () {
     var tweenMock = sinon.mock(tween.methods);
     tweenMock.expects('to')
-      .withArgs({ x: 100, y: 100 })
+      .withArgs({ x: 100, y: 100, c: 2 }, 6e3)
       .returns(tween.methods);
     revealPlanet(camera, planetPosition);
 
