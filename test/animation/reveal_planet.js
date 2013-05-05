559476e34a516dcf22f22d7d83252aa7e7266a95
Carl Whittaker <carl359@gmail.com>
2013-05-05 19:45:30 +0100

    Nix tests

--- test/animation/reveal_planet.js
+++ test/animation/reveal_planet.js
@@ -1,39 +1,3 @@
 describe('animation/revealPlanet', function () {
-  var tween = require('tween'),
-      planetPosition = { x: 100, y: 100 },
-      camera = { position: { x: 200, y: 200, z: 100, c: 0 } },
-      revealPlanet = require('../../lib/animation/reveal_planet');
 
-  it("starts from the cameras current position", function () {
-    sinon.spy(tween, 'Tween');
-    revealPlanet(camera, planetPosition);
-
-    tween.Tween.should.have.been.calledWith(camera.position);
-  });
-
-  it("tweens to the planet's x + y", function () {
-    var tweenMock = sinon.mock(tween.methods);
-    tweenMock.expects('to')
-      .withArgs({ x: 100, y: 100, c: 2 }, 5e3)
-      .returns(tween.methods);
-    revealPlanet(camera, planetPosition);
-
-    tweenMock.verify();
-  });
-
-  it("starts the animation", function () {
-    var tweenMock = sinon.mock(tween.methods);
-    tweenMock.expects('start');
-    revealPlanet(camera, planetPosition);
-
-    tweenMock.verify();
-  });
-
-  it("returns the tween instance", function () {
-    var returnStub = sinon.stub(),
-        tweenStub = sinon.stub(tween.methods, 'start')
-          .returns(returnStub);
-
-    revealPlanet(camera, planetPosition).should.equal(returnStub);
-  });
 });
