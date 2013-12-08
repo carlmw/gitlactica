9da342caa6e3f17032f9a515bc233a4f511afd07
Carl Whittaker <carl359@gmail.com>
2013-12-08 19:09:24 +0000

    Make tractor beam more transparent

--- adapters/beam.js
+++ adapters/beam.js
@@ -4,16 +4,15 @@ var THREE = require('three'),
 module.exports = beam;
 
 function beam () {
-  tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
-
   var mat = new THREE.MeshBasicMaterial({
         color: 0x00FFFC,
         map: tex,
         blending: THREE.AdditiveBlending,
-        transparent: true
+        transparent: true,
+        opacity: 0.25
       }),
-      geo = new THREE.PlaneGeometry(40, 5000, 1, 1);
-  var mesh = new THREE.Mesh(geo, mat);
+      geo = new THREE.PlaneGeometry(40, 5000, 1, 1),
+      mesh = new THREE.Mesh(geo, mat);
   mesh.doubleSided = true;
   mesh.rotation.z = Math.PI * 1.5;
   mesh.translateY(2500);
