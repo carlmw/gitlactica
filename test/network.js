d7e59831398f72a4d52bebf49dce7ef16af2170a
Carl Whittaker <carl359@gmail.com>
2014-01-18 18:35:09 +0000

    Add repo data to the commit event
    * Joins repo and commit requests into a single promise

--- test/network.js
+++ test/network.js
@@ -1,4 +1,5 @@
 var network = require('../lib/network'),
+    when = require('when'),
     moment = require('moment'),
     then = function (data) {
       return {
@@ -138,6 +139,8 @@ describe('network', function () {
 
           beforeEach(function () {
             clock = sinon.useFakeTimers();
+            sinon.stub(git, 'commit').returns(then({}));
+            sinon.stub(git, 'repo').returns(then({}));
             sinon.stub(git, 'events').returns(then([
               {
                 type: 'PushEvent',
@@ -157,29 +160,32 @@ describe('network', function () {
           });
 
           it('pops the oldest commit and requests its details', function () {
-            sinon.stub(git, 'commit').returns(then({}));
-
             subspace.emit('fetch:events');
 
             clock.tick(10e3);
 
             expect(git.commit).to.have.been.calledTwice;
+            expect(git.repo).to.have.been.calledTwice;
             expect(git.commit.args[0]).to.deep.equal(['carlmw/gitlactica', '8b07ccd197085a2c9aac1cc04aef93750aafd49d']);
+            expect(git.repo.args[0]).to.deep.equal(['carlmw/gitlactica']);
             expect(git.commit.args[1]).to.deep.equal(['carlmw/gitlactica', 'd94709d1942c14fe4bd06e24e9639ed30232b58e']);
+            expect(git.repo.args[1]).to.deep.equal(['carlmw/gitlactica']);
           });
 
           describe('and the commit request is successful', function () {
-            it('emits the commit event', function () {
-              sinon.stub(git, 'commit').returns(then({ commit: 'stuff' }));
-
+            it('emits the commit event', function (done) {
+              git.commit.returns(when({ commit: 'stuff' }));
+              git.repo.returns(when({ repo: 'things' }));
               var commitSpy = sinon.spy();
               subspace.on('commit', commitSpy);
-
+              subspace.on('commit', function () {
+                expect(commitSpy).to.have.been.calledOnce;
+                expect(commitSpy).to.have.been.calledWith({ commit: 'stuff' }, { repo: 'things' });
+                done();
+              });
               subspace.emit('fetch:events');
 
               clock.tick(5e3);
-              expect(commitSpy).to.have.been.calledOnce;
-              expect(commitSpy).to.have.been.calledWith({ commit: 'stuff' }, 'carlmw/gitlactica');
             });
           });
         });
