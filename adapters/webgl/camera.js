9509b05dd2e1c185f3fadb48167486610284d93a
Carl Whittaker <carl359@gmail.com>
2014-04-26 22:11:47 +0100

    Smooth camera lerp and slerp

--- adapters/webgl/camera.js
+++ adapters/webgl/camera.js
@@ -3,6 +3,7 @@ var THREE = require('three'),
 
 module.exports = function camera (cam) {
   var cameraPosition = cam.position.clone(),
+      lastTs,
       targetMat = new THREE.Matrix4(),
       targetQuat = new THREE.Quaternion(),
       cameraLookAt = new THREE.Vector3(0, 40000, 0),
@@ -13,22 +14,25 @@ module.exports = function camera (cam) {
 
   return {
     moveCamera: function (x, y, z, d) {
-      positionSpeed = d;
+      positionSpeed = d * 15;
       cameraPosition.set(x, y, z);
     },
     lookTo: function (x, y, z, d) {
-      lookAtSpeed = d;
+      lookAtSpeed = d * 15;
       cameraLookAt.set(x, y, z);
     }
   };
 
-  function update () {
+  function update (ts) {
     raf(update);
 
-    cam.position.lerp(cameraPosition, positionSpeed);
+    var rate = ((ts - lastTs) / 1000) || 1;
+    lastTs = ts;
+
+    cam.position.lerp(cameraPosition, positionSpeed * rate);
     targetMat.lookAt(cam.position, cameraLookAt, cam.up);
     targetQuat.setFromRotationMatrix(targetMat);
 
-    cam.quaternion.slerp(targetQuat, lookAtSpeed);
+    cam.quaternion.slerp(targetQuat, lookAtSpeed * rate);
   }
 };
