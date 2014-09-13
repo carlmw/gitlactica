543314190fe80750e7468e00e5c7e8cd28c5a5c5
Carl Whittaker <carl359@gmail.com>
2014-09-13 17:07:34 +0200

    Handle redirects when fetching languages

--- tasks/languages.js
+++ tasks/languages.js
@@ -1,6 +1,6 @@
 var jsyaml = require('js-yaml'),
     languageColourMapper = require('../lib/language_colour_mapper'),
-    https = require('https'),
+    request = require('request'),
     fs = require('fs');
 
 module.exports = function(grunt) {
@@ -8,21 +8,16 @@ module.exports = function(grunt) {
     var done = this.async(),
         YAML_URL = "https://raw.github.com/github/linguist/master/lib/linguist/languages.yml";
 
-    https.get(YAML_URL, function (res) {
-      var body = '';
-      res.on('data', function (d) {
-        body += d;
-      });
-      res.on('end', function () {
-        var languages = jsyaml.safeLoad(body),
-            colours = languageColourMapper(languages);
+    request.get(YAML_URL, function (err, res, body) {
+      if (err) {
+        grunt.log.write(err).error();
+      }
+      var languages = jsyaml.safeLoad(body),
+          colours = languageColourMapper(languages);
 
-        fs.writeFileSync('colours.json', JSON.stringify(colours));
-        grunt.log.writeln('colours.json output successfully').ok();
-        done();
-      });
-    }).on('error', function (e) {
-      grunt.log.write(e.message).error();
+      fs.writeFileSync('colours.json', JSON.stringify(colours));
+      grunt.log.writeln('colours.json output successfully').ok();
+      done();
     });
   });
 };
